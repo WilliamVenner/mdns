d51e48112fda617b39b0c57306a7a81dc5adee5e
William Venner <14863743+WilliamVenner@users.noreply.github.com>
2022-11-26 16:54:02 +0000

    Fix interval delay

--- src/runtime/async_std.rs
+++ src/runtime/async_std.rs
@@ -1,6 +1,7 @@
 use crate::Error;
 
 use async_std::future::TimeoutError;
+use async_std::stream::StreamExt;
 use futures_core::{Future, Stream};
 use std::{sync::Arc, time::Duration};
 
@@ -8,7 +9,7 @@ pub use async_std::net::UdpSocket as AsyncUdpSocket;
 pub use async_std::task::spawn;
 
 pub fn create_interval_stream(request_interval: Duration) -> impl Stream<Item = ()> {
-    async_std::stream::interval(request_interval)
+    async_std::stream::once(()).chain(async_std::stream::interval(request_interval))
 }
 
 pub fn make_async_socket(socket: std::net::UdpSocket) -> Result<Arc<AsyncUdpSocket>, Error> {
--- src/runtime/tokio.rs
+++ src/runtime/tokio.rs
@@ -1,16 +1,16 @@
 use crate::Error;
 
 use futures_core::{Future, Stream};
+use tokio_stream::StreamExt;
 use std::{sync::Arc, time::Duration};
-use tokio::time::Instant;
 
 pub use tokio::net::UdpSocket as AsyncUdpSocket;
 pub use tokio::spawn;
-pub fn create_interval_stream(request_interval: Duration) -> impl Stream<Item = Instant> {
-    tokio_stream::wrappers::IntervalStream::new(tokio::time::interval_at(
+pub fn create_interval_stream(request_interval: Duration) -> impl Stream<Item = ()> {
+    tokio_stream::once(()).chain(tokio_stream::wrappers::IntervalStream::new(tokio::time::interval_at(
         tokio::time::Instant::now() + request_interval,
         request_interval,
-    ))
+    )).map(|_| ()))
 }
 
 pub fn make_async_socket(socket: std::net::UdpSocket) -> Result<Arc<AsyncUdpSocket>, Error> {
